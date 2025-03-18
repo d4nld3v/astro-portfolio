8fecadfad181e0a378e9fe0f2b636641ec8b071e
d4nld3v <danielenriquezdeveloper@outlook.com>
2025-03-18 16:44:27 +0100

    Update base path in Astro configuration to relative path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://d4nld3v.github.io/astro-portfolio/',
-  base: '/',
+  base: './',
   output: 'static',
 });
