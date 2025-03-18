da1ad5cf5f39a6126f038ed7dc3956826dcb83b4
d4nld3v <danielenriquezdeveloper@outlook.com>
2025-03-18 16:33:25 +0100

    Remove base path from Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://d4nld3v.github.io/astro-portfolio/',
-  base: '/astro-portfolio/',
   output: 'static',
 });
