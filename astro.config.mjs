a735ecb09fa9d9c5ee0ef01a54446c3f941e693e
d4nld3v <danielenriquezdeveloper@outlook.com>
2025-03-19 11:47:20 +0100

    Add new font and SVG assets for documentation and update Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://d4nld3v.github.io/astro-portfolio/',
-  output: 'static',
-  base: '/astro-portfolio/', // Set the base path for GitHub Pages
+  base: './',
+  outDir: './docs',
+  output: 'static'
 });
