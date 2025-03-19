96323f6850fe1bc09e0b2d55b9b814c22321b739
d4nld3v <danielenriquezdeveloper@outlook.com>
2025-03-19 11:51:56 +0100

    Update Astro configuration for GitHub Pages deployment and remove unused CSS file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://d4nld3v.github.io/astro-portfolio/',
-  base: './',
+  site: 'https://d4nld3v.github.io',
+  base: '/astro-portfolio/',
   outDir: './docs',
-  output: 'static'
+  output: 'static',
+  build: {
+    format: 'file'
+  }
 });
