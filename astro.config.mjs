858d606eb86a6e41f6ec3189cc146083bd22f46b
d4nld3v <danielenriquezdeveloper@outlook.com>
2025-03-19 11:43:58 +0100

    Update Astro configuration to set base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://d4nld3v.github.io/astro-portfolio/',
-  base: '/astro-portfolio/',
   output: 'static',
-  trailingSlash: 'always',
+  base: '/astro-portfolio/', // Set the base path for GitHub Pages
 });
